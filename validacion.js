ffb267bd186002eba34e50c2c3952b081d0a6af3
LeonardoFMR <157821835+LeonardoFMR@users.noreply.github.com>
2024-04-29 21:51:47 -0600

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,9 @@
+function showLinkedinBadge() {
+    const linkedinBadge = document.getElementById('linkedinBadge');
+    linkedinBadge.style.display = 'block';
+}
+
+function hideLinkedinBadge() {
+    const linkedinBadge = document.getElementById('linkedinBadge');
+    linkedinBadge.style.display = 'none';
+}
